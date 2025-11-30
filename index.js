2d736a76179dda1505c96bfa23de54187c06ef12
bank <bku850438@gmail.com>
2025-11-30 13:46:50 +0700

    t

--- index.js
+++
@@ -1,9 +0,0 @@
-const nav = document.getElementById("navContent")
-const button = document.getElementById("closeButton")
-window.addEventListener("load",()=>{
-      nav.classList.add('animate-slide-bounce');
-})
-
-button.addEventListener("click",()=>{
-    nav.classList.remove("animate-slide-bounce")
-})
